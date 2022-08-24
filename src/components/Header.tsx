63162b73632a8b24dfa90a0124f51076ef3768cc
Thefederico <federico_upegui.dev@outlook.com>
2022-08-24 18:02:19 -0500

    chore: User menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { AppContext } from '../context/App.context'
+import { NavLink } from 'react-router-dom'
 
 const Header: React.FC = () => {
+  const { logout } = useContext(AppContext)
+  const [menu, setMenu] = useState(false)
+
+  const handleLogout = async (): Promise<void> => {
+    await logout()
+  }
+
   return (
-    <header className='flex top-0 z-50 sticky px-4 py-2 shadow-sm bg-prymaryDark'>
-      <figure className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
-        <svg
-          className='w-12 h-auto text-ferrariRed cursor-pointer'
-          fill='currentColor'
-          viewBox='0 0 20 20'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path d='M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z' />
-        </svg>
+    <header className='flex top-0 z-50 sticky px-3 py-2 shadow-sm bg-primaryDark'>
+      <figure className='flex items-center my-1 relative h-10 mx-2  flex-shrink-0 cursor-pointer'>
+        <NavLink to='/'>
+          <svg
+            className='w-12 h-auto text-ferrariRed cursor-pointer'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z' />
+          </svg>
+        </NavLink>
+        <p className='hidden lg:block text-white'>Videoteca</p>
       </figure>
-      <form className='flex flex-1 items-center space-x-2 border border-gray-700 rounded-sm bg-secondaryDark px-3 py-1'>
+      <form className='flex lg:mx-40 flex-1 items-center space-x-2 border border-gray-700 rounded-sm bg-secondaryDark px-2 py-1s '>
         <svg
           className='w-6 h-auto text-textInputField'
           fill='currentColor'
@@ -33,12 +45,13 @@ const Header: React.FC = () => {
         />
         <button type='submit' hidden />
       </form>
-      <section className='lg:ml-96'>
+      <section className='flex'>
         <svg
-          className='w-12 h-auto text-veryLightPink cursor-pointer'
+          className='w-12 h-auto  text-white cursor-pointer'
           fill='currentColor'
           viewBox='0 0 20 20'
           xmlns='http://www.w3.org/2000/svg'
+          onClick={() => setMenu(prevstate => !prevstate)}
         >
           <path
             fillRule='evenodd'
@@ -46,6 +59,18 @@ const Header: React.FC = () => {
             clipRule='evenodd'
           />
         </svg>
+        <ul
+          className={`w-full ${
+            !menu ? 'hidden' : 'block'
+          } absolute right-4 top-14 z-50 text-right text-white mt-3 `}
+        >
+          <li className='hover:underline cursor-pointer'>
+            <NavLink to='/account'>Account</NavLink>
+          </li>
+          <li className='hover:underline cursor-pointer'>
+            <a onClick={handleLogout}>Exit</a>
+          </li>
+        </ul>
       </section>
     </header>
   )
