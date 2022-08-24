54cefa2c08c3117562ccdb41645fa7de080b0260
Thefederico <federico_upegui.dev@outlook.com>
2022-08-24 18:01:37 -0500

    feat: App router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import { Layout } from './layout'
 import { Home } from './pages/Home'
-// import { Login } from './pages/Login'
+import { Login } from './pages/Login'
+import { AppProvider } from './context/App.context'
+import { Register } from './pages/Register'
+import { Account } from './pages/Account'
+import { ContentDetail } from './components/ContentDetails'
+import { useVideos } from './hooks/useVideos.hook'
 
 const App: React.FC = () => {
+  const { videos, generateFakeVideos } = useVideos()
+
+  useEffect(() => {
+    generateFakeVideos(10)
+  }, [])
+
+  console.log(videos)
+
   return (
-    <div>
-      {/* <Login /> */}
-      <Layout>
-        <Home />
-      </Layout>
-    </div>
+    <AppProvider>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path='/account'
+          element={
+            <Layout>
+              <Account />
+            </Layout>
+          }
+        />
+        <Route path='/detail' element={<ContentDetail />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Register />} />
+      </Routes>
+    </AppProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from '../src/app/store.js'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 )
